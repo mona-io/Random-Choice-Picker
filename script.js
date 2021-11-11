7ae2b5dcd3da460331311186b1ab76b89e94e463
mona-io <mona.aziminia88@gmail.com>
2021-11-11 19:06:04 +0330

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const tagsElement = document.getElementById('tags')
+const textArea = document.getElementById('textarea')
+
+textArea.focus()
+
+textArea.addEventListener('keyup', (e) => { 
+  createTags(e.target.value)
+})
+
+function createTags(input) {
+  const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+  tagsElement.innerHTML = ''
+
+  tags.forEach(tag => {
+    const tagEl = document.createElement('span')
+    tagEl.classList.add('tag')
+    tagEl.innerText = tag
+    tagsElement.appendChild(tagEl)
+  })
+}
+
+
+
