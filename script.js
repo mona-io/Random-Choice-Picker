b3f57a4eae745a136d83b8873cd17d6cca857959
mona-io <mona.aziminia88@gmail.com>
2021-11-12 19:51:39 +0330

    Final Changes

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
-const tagsElement = document.getElementById('tags')
+const tagsEl = document.getElementById('tags')
 const textArea = document.getElementById('textarea')
 
 textArea.focus()
 
-textArea.addEventListener('keyup', (e) => { 
+textArea.addEventListener('keyup', (e) => {
   createTags(e.target.value)
+
+  if(e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = ''
+    }, 10)
+    randomSelect()
+  }
 })
 
 function createTags(input) {
   const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
 
-  tagsElement.innerHTML = ''
+  tagsEl.innerHTML = ''
 
   tags.forEach(tag => {
     const tagEl = document.createElement('span')
     tagEl.classList.add('tag')
     tagEl.innerText = tag
-    tagsElement.appendChild(tagEl)
+    tagsEl.appendChild(tagEl)
   })
 }
 
+function randomSelect() {
+  const times = 30
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+    highlightTag(randomTag)
+    setTimeout(() => {
+      unHighlightTag(randomTag)
+    }, 100)
+  }, 100);
 
+  setTimeout(() => {
+    clearInterval(interval)
 
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+
+      highlightTag(randomTag)
+    }, 100)
+  }, times * 100)
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+  tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight')
+}
